36d413555711e16a40fbac6c2e8c88d688cfbf83
CosmoWorker <social.tarang@gmail.com>
2025-08-17 22:42:37 +0530

    added tokenId for increased entropy

--- server/jwt-utils.ts
+++ server/jwt-utils.ts
@@ -2,11 +2,17 @@ import jwt, { type SignOptions } from "jsonwebtoken";
 import { envConfig } from "./config";
 
 export const generateRefreshToken=(userId : string)=>{
-    return jwt.sign(userId, envConfig.SECRET_KEY, {expiresIn: envConfig.REFRESH_TOKEN_EXPIRY} as SignOptions)
+    const payload={
+        userId, tokenId: crypto.randomUUID()
+    }
+    return jwt.sign(payload, envConfig.SECRET_KEY, {expiresIn: envConfig.REFRESH_TOKEN_EXPIRY} as SignOptions)
 }
 
 export const generateAccessToken=(userId: string)=>{
-    return jwt.sign(userId, envConfig.SECRET_KEY, {expiresIn: envConfig.ACCESS_TOKEN_EXPIRY} as SignOptions)
+    const payload={
+        userId, tokenId: crypto.randomUUID()
+    }
+    return jwt.sign(payload, envConfig.SECRET_KEY, {expiresIn: envConfig.ACCESS_TOKEN_EXPIRY} as SignOptions)
 }
 
 export const verifyToken=(token: string, secret: string)=>{
