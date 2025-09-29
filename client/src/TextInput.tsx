196268e3b9aaa574b628f86e6f65520dfd0c714d
CosmoWorker <social.tarang@gmail.com>
2025-09-29 23:40:53 +0530

    added structured folders for page components

--- client/src/TextInput.tsx
+++ client/src/TextInput.tsx
@@ -1,13 +1,12 @@
 import type React from "react"
 
-type Props={value: string, onChange: (e: React.ChangeEvent<HTMLInputElement>)=>void, onClear: ()=>void}
+type Props={value: string, onChange: (e: React.ChangeEvent<HTMLInputElement>)=>void, ref: React.RefObject<HTMLInputElement | null>, keyDown: (e: React.KeyboardEvent<HTMLInputElement>)=> void}
 
-export const TextInput=({value, onChange, onClear}: Props)=>{
+export const TextInput=({value, onChange, ref, keyDown}: Props)=>{
 
     return(
         <>
-            {value && <button onClick={onClear}>Clear</button>}
-            <input type="text" placeholder='Enter anything...' value={value} onChange={onChange} className='border-1 rounded-md p-1.5 mr-2'/>
+            <input type="text" placeholder='Enter anything...' ref={ref} value={value} onChange={onChange} onKeyDown={keyDown} className='border-1 rounded-md p-1.5 mr-2'/>
         </>
     )
 }
